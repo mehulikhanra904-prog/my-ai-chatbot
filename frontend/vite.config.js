ffac3f9364ce3521f501bc9ae2d9c2c488dfa35a
MEHULI KHANRA <mehulikhanra904@gmail.com>
2026-08-28 22:31:15 +0530

    modified files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5000',
+        target: 'https://my-ai-chatbot-2-o2pz.onrender.com',
         changeOrigin: true,
         secure: false,
       },
